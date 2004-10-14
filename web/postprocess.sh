964d2bdf43be15b97623f172f0889ea85845fb41
ebruneton <ebruneton@271bd773-ee82-43a6-9b2b-1890ed8ce7f9>
2004-10-14 14:25:48 +0000

    *** empty log message ***
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/asm/trunk@352 271bd773-ee82-43a6-9b2b-1890ed8ce7f9

--- web/postprocess.sh
+++ web/postprocess.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 cd doc
-sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' tutorial-holder.html > tutorial.html
-sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' tutorial-holder_print.html > tutorial_print.html
+sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder.html > tutorial.html
+sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder_print.html > tutorial_print.html
