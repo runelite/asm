3264aefb8a2eafe6ca25f4bc8587cb51b3edc196
ebruneton <ebruneton@271bd773-ee82-43a6-9b2b-1890ed8ce7f9>
2004-10-25 07:21:39 +0000

    added new tutorial
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/asm/trunk@374 271bd773-ee82-43a6-9b2b-1890ed8ce7f9

--- web/postprocess.sh
+++ web/postprocess.sh
@@ -3,3 +3,6 @@
 cd doc
 sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder.html > tutorial.html
 sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder_print.html > tutorial_print.html
+
+sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder.html > tutorial-annotations.html
+sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder_print.html > tutorial-annotations_print.html
