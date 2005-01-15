08e4e343efcfd688196eb1b27acd6430ddcd9d18
(no author) <(no author)@271bd773-ee82-43a6-9b2b-1890ed8ce7f9>
2005-01-15 21:41:10 +0000

    This commit was manufactured by cvs2svn to create branch 'SIGNATURE_SUPPORT'.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/asm/branches/SIGNATURE_SUPPORT@470 271bd773-ee82-43a6-9b2b-1890ed8ce7f9

--- web/postprocess.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-cd doc
-sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder.html > tutorial.html
-sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder_print.html > tutorial_print.html
-
-sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder.html > tutorial-annotations.html
-sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder_print.html > tutorial-annotations_print.html
