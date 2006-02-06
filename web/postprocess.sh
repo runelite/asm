47d8fcd2f700e2bbd9ab36af0eed37e889754220
ebruneton <ebruneton@271bd773-ee82-43a6-9b2b-1890ed8ce7f9>
2006-02-06 10:19:01 +0000

    added developer guide
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/asm/trunk@957 271bd773-ee82-43a6-9b2b-1890ed8ce7f9

--- web/postprocess.sh
+++ web/postprocess.sh
@@ -9,3 +9,6 @@ sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d
 
 sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder.html > tutorial-annotations.html
 sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder_print.html > tutorial-annotations_print.html
+
+sed -e '/TOBEREPLACED/r developer-guide-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' -e 's/<\/head>/<style type="text\/css">pre { margin: 0px 0px 0px 0px; } p, li { margin-right: 0px; text-align: justify; }<\/style><\/head>/' developer-guide-holder.html > developer-guide.html
+sed -e '/TOBEREPLACED/r developer-guide-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' -e 's/<\/head>/<style type="text\/css">pre { margin: 0px 0px 0px 0px; } p, li { margin-right: 0px; text-align: justify; }<\/style><\/head>/' developer-guide-holder_print.html > developer-guide_print.html
