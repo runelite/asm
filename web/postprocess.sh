f785c008bb875cb1d27a6e4ebbdc481bc54abfc8
pcregut <pcregut@271bd773-ee82-43a6-9b2b-1890ed8ce7f9>
2012-04-01 16:09:38 +0000

    Tutorial for asmdex. Linked in documentation. Reference to Julien's master thesis. History updated.
    git-svn-id: svn://svn.forge.objectweb.org/svnroot/asm/trunk@1618 271bd773-ee82-43a6-9b2b-1890ed8ce7f9

--- web/postprocess.sh
+++ web/postprocess.sh
@@ -7,6 +7,9 @@ sed -e '/TOBEREPLACED/r tutorial-asm-2.0-body.html' -e '/TOBEREPLACED/d' -e '/!D
 sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder.html > tutorial.html
 sed -e '/TOBEREPLACED/r tutorial-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-holder_print.html > tutorial_print.html
 
+sed -e '/TOBEREPLACED/r tutorial-asmdex-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-asmdex-holder.html > tutorial-asmdex.html
+sed -e '/TOBEREPLACED/r tutorial-asmdex-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-asmdex-holder_print.html > tutorial-asmdex_print.html
+
 sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder.html > tutorial-annotations.html
 sed -e '/TOBEREPLACED/r tutorial-annotations-body.html' -e '/TOBEREPLACED/d' -e '/!DOCTYPE/d' tutorial-annotations-holder_print.html > tutorial-annotations_print.html
 
